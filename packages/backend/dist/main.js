ff905e49cc71833034be0fa64e881af01a2b4cb1
osmandlsmn <106361546+osmandlsmn@users.noreply.github.com>
2022-10-17 21:30:30 +0300

    Fix errors

--- packages/backend/dist/main.js
+++ packages/backend/dist/main.js
@@ -4,9 +4,9 @@ const core_1 = require("@nestjs/core");
 const common_1 = require("@nestjs/common");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: { origin: ['http://localhost:5173', 'http://localhost:3000'] } });
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: { origin: '*' } });
     app.useGlobalPipes(new common_1.ValidationPipe());
-    await app.listen(5000);
+    await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -3,8 +3,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: { origin: ['http://localhost:5173', 'http://localhost:3000'] } });
+  const app = await NestFactory.create(AppModule, { cors: { origin: '*' } });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
