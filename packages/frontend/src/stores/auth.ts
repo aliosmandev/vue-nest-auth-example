43da7add730d4ee8266f9e6014140f7939dab528
osmandlsmn <106361546+osmandlsmn@users.noreply.github.com>
2022-10-20 21:24:13 +0300

    Update Auth Store

--- packages/frontend/src/stores/auth.ts
+++ packages/frontend/src/stores/auth.ts
@@ -1,23 +1,24 @@
-import type { User, LoginPayload } from "@/types";
-import { defineStore } from "pinia";
+import type { User, LoginBody } from "@/types";
 import { ref, computed } from "vue";
+import { defineStore } from "pinia";
+import { useRouter } from "vue-router";
+import { useToast } from "vue-toastification";
 import axios from "@/plugins/axios";
 
 export const useAuthStore = defineStore("auth", () => {
+  const toast = useToast();
+  const router = useRouter();
   const user = ref<User | null>();
   const isAuthenticated = computed<boolean>(() => !!user.value);
-  const errorMessage = ref();
 
-  const login = async (payload: LoginPayload): Promise<boolean> => {
-    errorMessage.value = "";
+  const login = async (payload: LoginBody) => {
     try {
       const response = await axios.post("/auth/login", payload);
       user.value = response.data.user;
       localStorage.setItem("token", response.data.accessToken);
-      return true;
+      router.replace("/dashboard");
     } catch (err: any) {
-      errorMessage.value = err.response.data.message;
-      return false;
+      toast.error(err.response.data.message);
     }
   };
 
@@ -31,5 +32,5 @@ export const useAuthStore = defineStore("auth", () => {
     user.value = response.data;
   };
 
-  return { user, isAuthenticated, errorMessage, login, logout, me };
+  return { user, isAuthenticated, login, logout, me };
 });
